ffc1c04a78332bb91a7687a70fbcf39546711991
Nathan Edwards <npfedwards@gmail.com>
2012-06-19 12:35:09 +0100

    Delete login.php

--- login.php
+++
@@ -1,5 +0,0 @@
-<form action='dologin.php' method='post'>
-	<label for='username'>Username</label><input type='text' name='username' id='username'><br>
-    <label for='password'>Password</label><input type='password' name='password' id='password'><br>
-    <input type='submit' value='Login'>
-</form>
