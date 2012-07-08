e4f462b3263d75c59cf0cb6744aa96a9f7b3e51c
Nathan Edwards <npfedwards@gmail.com>
2012-07-08 21:55:27 +0100

    Fix #66

--- xmlhttp/updatetotal.php
+++ xmlhttp/updatetotal.php
@@ -7,11 +7,15 @@
 	$result=mysql_query($query) or die(mysql_error());
 	$total=0;
 	
+	$cs=currencysymbol($user);
+	
 	while($row=mysql_fetch_assoc($result)){
 		$total=$total+$row['PaymentAmount'];
 	}
 	if($total<0){
-		$total="<span class='red'>".$total."</span>";
+		$total="<span class='red'>".$cs.$total."</span>";
+	}else{
+		$total=$cs.$total;	
 	}
 	
 	echo $total;
