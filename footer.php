dbca5ff0992ea1ef75e9d1c569f17bf4315ac17f
Nathan Edwards <npfedwards@gmail.com>
2012-07-12 11:42:19 +0100

    Alpha setup

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
 		<?php
             closedb($conn);
         ?>
+        	<div id='footer'>
+            	This is an alpha version of this app. That means it's still in the early stages, it works but is still missing a lot of features and we could still break it. So please use, but don't stake too much on your data surviving any edits (though it should!) and give us feedback with bugs and features you want to see at our <a href='https://github.com/npfedwards/Financial-Management-App/issues'>Github Repo</a>
+            </div>
     	</div>
 	</body>
 </html>
