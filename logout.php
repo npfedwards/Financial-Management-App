f365e2e5d1eb1d7f6b3e8d106edecd267e883501
Nathan Edwards <npfedwards@gmail.com>
2013-02-06 17:21:38 +0000

    CodeIgniter... start all over again

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	setcookie("userid", "", time()-3600);
-	setcookie("sessionkey", "", time()-3600);
-	
-	//Maybe should do something to the db too
-	
-	$loggedin=0;
-?>
-Logged out. Go to <a href='index.php'>Home</a>
